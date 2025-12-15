3601fc4353f44689d08032afcd13f1da60f7daff
skyww0 <Skyesomaya05@gmail.com>
2025-12-15 16:28:59 +0400

    Create 24_mixtape.py

---
+++ 24_mixtape.py
@@ -0,0 +1,9 @@
+playlist = ['Johannes Brahms - Symphony No.1 in C Minor, Op. 68', 
+            'Erik Satie - Gymnopédie No.1', 
+            'Tchaikovsky - 1812 Overture',
+            'Edvard Grieg - In the Hall of the Mountain King',
+            'Ludwig van Beethoven - Moonlight Sonata', 
+            'Antonio Vivaldi - The Four Seasons']
+
+for song in playlist:
+  print(song)
